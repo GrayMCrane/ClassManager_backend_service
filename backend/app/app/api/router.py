21216212c15a0537e65260fea6b30ac5c0fea322
gray <l87894741@gmail.com>
2021-09-10 15:46:18 +0800

    style: little style change

--- backend/app/app/api/router.py
+++ backend/app/app/api/router.py
@@ -5,13 +5,15 @@ from app.api.class_manager import (
 )
 
 api_router = APIRouter()
-api_router.include_router(login.router, tags=["login"])
-api_router.include_router(pages.router, prefix='/pages', tags=['pages'])
+api_router.include_router(classes.router, prefix='/classes', tags=['classes'])
 api_router.include_router(
     configurations.router, prefix='/configurations', tags=['configurations']
 )
-api_router.include_router(classes.router, prefix='/classes', tags=['classes'])
-api_router.include_router(users.router, prefix='/users', tags=['users'])
 api_router.include_router(
     feedbacks.router, prefix='/feedbacks', tags=['feedbacks']
 )
+api_router.include_router(login.router, tags=["login"])
+api_router.include_router(pages.router, prefix='/pages', tags=['pages'])
+
+api_router.include_router(users.router, prefix='/users', tags=['users'])
+
